d8d845a80cf1d66c784bcb4c94997a37a647f019
cuiyinguniverse <124242347+cuiyinguniverse@users.noreply.github.com>
2024-04-16 18:47:13 +0800

    Update download.sh

--- linux shell/download.sh
+++ linux shell/download.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+#download from NCBI
+
+wget -c -o ./yangh22/Pseudomonas_aeruginosa.zip https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/download?filename=ncbi_dataset.zip&ncbi_phid=939B2E32DA0F9385000039011AE78026.1.m_1.055 
