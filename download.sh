9d0348c025c1430bf5595127195e168aa82fd81a
cuiyinguniverse <124242347+cuiyinguniverse@users.noreply.github.com>
2024-04-16 17:36:24 +0800

    Delete download.sh

--- download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#download from NCBI
-wget -c -o ./yangh22/Pseudomonas_aeruginosa.zip https://api.ncbi.nlm.nih.gov/datasets/v2alpha/genome/download?filename=ncbi_dataset.zip&ncbi_phid=939B2E32DA0F9385000039011AE78026.1.m_1.055 
